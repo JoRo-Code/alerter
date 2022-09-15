b42cbc89deed3ca76a88ace0398b39f1e9191c79
Jonas Rosengren <rosengrenjonas98@gmail.com>
2022-09-15 20:48:12 +0200

    verify correct checked time period for slots

--- checks/slots.py
+++ checks/slots.py
@@ -6,13 +6,16 @@ import traceback
 from datetime import datetime
 
 DEBUG = True
+TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+# placeholder for checked date
+checkedStartDate = ""
 
 def calcTicks():
     """
     bokadirekt uses their own time measurement. 
     Check this function if time interval is inaccurate
     """
-    TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
     baseDate, baseTicks = ("2022-08-21T1:00:00", 1661040000000)
     baseTime = datetime.strptime(baseDate, TIME_FORMAT)
     now = datetime.today()
@@ -25,6 +28,23 @@ def calcTicks():
 
     return ticks
 
+def getWeek(date:datetime) -> int:
+    return date.isocalendar()[1]
+
+def verifyCheckedPeriod(rawDate:str) -> Exception:
+    """
+    Checks if rawDate is corresponding to the current week
+    """
+    startDate = datetime.strptime(rawDate.split("+")[0], TIME_FORMAT)
+    startWeek = getWeek(startDate)
+    currentWeek = getWeek(datetime.now())
+    
+    #if DEBUG: print(f"   * Checked slots from: {startDate} (week {startWeek})")
+    if startWeek != currentWeek:
+        raise Exception(
+            "WRONG SLOT PERIOD: Checked slot period is not corresponding to current week. " + 
+            f"Check starting from week '{startWeek}' and not '{currentWeek}' ({startDate}). Check calcTicks-function.")
+    
 def getUrl(serviceId):
     ticks = calcTicks()
     url = f"https://www.bokadirekt.se/api/book/{(serviceId)}/12384/{ticks}/10650?reborn=true"
@@ -51,11 +71,21 @@ def getJson(serviceId):
 
 def parse(data, serviceId):
     try:
-        result = data["availability"]["fromErpOverview"]["datesWithOpeningHours"]
+        overview = data["availability"]["fromErpOverview"]
+        result = overview["datesWithOpeningHours"]
+        
+        
     except Exception:
         exc_type, value, tb = sys.exc_info()
         raise ParseException(str(serviceId) + ": " + str(exc_type.__name__) + ": " + str(value) + str(traceback.extract_tb(tb)))
     else:
+        # verify checked week is correct
+        global checkedStartDate 
+        startDate = overview['minDateChecked']
+        checkedStartDate = startDate
+        
+        verifyCheckedPeriod(startDate)
+        
         return result
 
 
@@ -69,6 +99,8 @@ def fetchSlots():
     for serviceTitle, serviceId in services.items():
         if DEBUG: print(f" - Fetching service: '{serviceTitle}' with ID: '{serviceId}'")
         availableSlots += fetchSlotsByService(serviceId)
+    
+    if DEBUG: print(f" - Checked slots from '{checkedStartDate}'")
     
     return availableSlots
 
